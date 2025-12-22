f7f6102a7d963f54c1d8549de4fa424d894f5009
Alina Salmanova <alina.salmanova@netcracker.com>
2025-12-22 06:08:08 +0000

    fix

--- frontend_103/lesson_2/js/main.js
+++ frontend_103/lesson_2/js/main.js
@@ -1,15 +1,11 @@
-function isBigEnough(value) {
-  return value >= 40;
-};
-
 const products = [
     { name: "Яблоко", price: 50 },
     { name: "Банан", price: 30 },
     { name: "Апельсин", price: 70 },
 ];
 
-const increased = products.map(fruit => fruit.price * 1.1);
-const expensiveProducts = products.filter(product => product.price > 40);
+const increased = products.map(fruit => fruit.price * 1.1); // increase price 10%
+const expensiveProducts = products.filter(product => product.price > 40); // search price more 40
 
 console.log(expensiveProducts);
 console.log(increased);
